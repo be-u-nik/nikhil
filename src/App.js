8f32d263d21206bff448598a0caad5e004b15ff8
be-u-nik <nikhil2001rockz@gmail.com>
2022-02-16 13:25:20 +0530

    home 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
-import { FaHome, FaHamburger } from "react-icons/fa";
+import { FaHome, FaHamburger, FaGraduationCap, FaCode } from "react-icons/fa";
+import { BsFillEmojiLaughingFill } from "react-icons/bs";
+import { BiMessageRoundedDetail } from "react-icons/bi";
+import homeImage from "./assets/images/nikhil_icon-removebg-preview.png";
 function App() {
   return (
     <div className="App">
@@ -8,10 +11,10 @@ function App() {
           <div className="col-12">
             {/* waves */}
             {/* Content before waves */}
-            <div className="inner__container d-flex flex-column flex-md-row mx-2 align-items-center">
+            <div className="inner__container d-flex flex-column flex-lg-row mx-2 align-items-center">
               {/* icons left */}
-              <div className="wrapper d-flex flex-md-column justify-content-start justify-content-md-center align-self-start align-self-md-center col-2 col-md-1">
-                <div className="d-md-none menu_item facebook">
+              <div className="wrapper d-flex flex-lg-column justify-content-start justify-content-lg-center align-self-start align-self-lg-center col-2 col-lg-1">
+                <div className="d-lg-none menu_item facebook">
                   <div
                     className="icon"
                     onClick={() => {
@@ -25,96 +28,102 @@ function App() {
                     <FaHamburger />
                   </div>
                 </div>
-                <div className="d-none d-md-block" id="menu_items">
+                <div className="d-none d-lg-block" id="menu_items">
                   <div className="menu_item facebook">
                     <div className="icon">
-                      <FaHome />
+                      <a href="#home" className="menu_item_icon">
+                        <FaHome />
+                      </a>
                     </div>
-                    <div className="tooltip tooltip-left d-none d-md-block">
-                      Facebook
+                    <div className="tooltip tooltip-left d-none d-lg-block">
+                      Home
                     </div>
                   </div>
                   <div className="menu_item twitter">
                     <div className="icon">
-                      <FaHome />
+                      <a href="#about" className="menu_item_icon">
+                        <BsFillEmojiLaughingFill />
+                      </a>
                     </div>
                     <div className="tooltip tooltip-left d-none d-md-block">
-                      Twitter
+                      About
                     </div>
                   </div>
                   <div className="menu_item instagram">
                     <div className="icon">
-                      <FaHome />
+                      <a href="#resume" className="menu_item_icon">
+                        <FaGraduationCap />
+                      </a>
                     </div>
                     <div className="tooltip tooltip-left d-none d-md-block">
-                      Instagram
+                      Resume
                     </div>
                   </div>
                   <div className="menu_item github">
                     <div className="icon">
-                      <FaHome />
+                      <a href="#projects" className="menu_item_icon">
+                        <FaCode />
+                      </a>
                     </div>
                     <div className="tooltip tooltip-left d-none d-md-block">
-                      Github
+                      Projects
                     </div>
                   </div>
                   <div className="menu_item youtube">
                     <div className="icon">
-                      <FaHome />
+                      <a href="#contact_me" className="menu_item_icon">
+                        <BiMessageRoundedDetail />
+                      </a>
                     </div>
                     <div className="tooltip tooltip-left d-none d-md-block">
-                      Youtube
-                    </div>
-                  </div>
-                  <div className="menu_item youtube">
-                    <div className="icon">
-                      <FaHome />
-                    </div>
-                    <div className="tooltip tooltip-left d-none d-md-block">
-                      Youtube
-                    </div>
-                  </div>
-                  <div className="menu_item youtube">
-                    <div className="icon">
-                      <FaHome />
-                    </div>
-                    <div className="tooltip tooltip-left d-none d-md-block">
-                      Youtube
+                      Contact Me
                     </div>
                   </div>
                 </div>
               </div>
 
               <div className="content col-12 col-md-10">
-                <section>
-                  <h1>Home</h1>
+                <section id="home">
+                  <div className="d-flex flex-column align-items-center flex-lg-row align-items-lg-start w-100 h-100">
+                    <div className="order-2 order-lg-1 d-flex flex-column align-items-center align-items-lg-start text-left home__content col-12 col-lg-7">
+                      <h1>Hi</h1>
+                      <h3>I am Nikhil Masigari</h3>
+                      <h4>Web developer |</h4>
+                    </div>
+                    <div className="col-12 col-lg-5 mb-3 mb-lg-none order-1 order-lg-2 d-flex justify-content-center">
+                      <div className="home__image rounded-circle">
+                        <img
+                          src={homeImage}
+                          alt="nikhil"
+                          className="rounded-circle"
+                        />
+                      </div>
+                    </div>
+                  </div>
                 </section>
-                <section>
+                <section id="about">
                   <h1>About</h1>
                 </section>
-                <section>
+                <section id="resume">
                   <h1>Resume</h1>
                 </section>
-                <section>
+                <section id="projects">
                   <h1>Projects</h1>
                 </section>
-                <section>
-                  <h1>Testimonials</h1>
-                </section>
-                <section>
+                <section id="contact_me">
                   <h1>Contact me</h1>
                 </section>
               </div>
             </div>
             {/* Waves Container */}
-            <div style={{ zIndex: "0" }}>
+            <div style={{ zIndex: "1" }}>
               <svg
                 className="waves"
                 xmlns="http://www.w3.org/2000/svg"
                 xmlnsXlink="http://www.w3.org/1999/xlink"
                 viewBox="0 24 150 28"
                 preserveAspectRatio="none"
-                shape-rendering="auto"
+                shapeRendering="auto"
               >
                 <defs>
                   <path
